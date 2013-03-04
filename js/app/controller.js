2bdcd6fa9fcf87f05e7c94efa7681e2a660e02e2
peec <kjelkenes@gmail.com>
2013-03-04 21:15:07 +0100

    Mer kommentarer.

--- js/app/controller.js
+++ js/app/controller.js
@@ -1,10 +1,33 @@
+/**
+ * Forklaring av denne modul strukturen.
+ * Marionette har laget en enkelt arkitektur på å lage moduler.
+ * En modul defineres med AppVariabel.module("Mitt.Namespace.Objekt", funksjon);
+ * 
+ * Marionette oppretter modul objektet når appen starter og passer argumenter til modulen:
+ * 
+ * Modul instansen
+ *   - Merk, vi kan bygge videre på denne, slik som vi gjør i denne "Controlleren".
+ * Applikasjon instansen
+ * Backbone objektet
+ * Marionette objektet
+ * jQuery
+ * Underscore
+ * 
+ * 
+ */
 FaceApp.module("Controller", function(Controller, FaceApp, Backbone, Marionette, $, _){
 
 	
 
+	// En kontroller sin metode, tenk Ruter -> Controller.metode.
 	Controller.index = function(){
 		console.log("Controller.index running.");
+		
+		// Lengre nede i denne filen registrerer vi FaceApp.layout i en initializer.
+		// Husk at initializer blir kjørt før denne koden, så den vil bli tilgjengelig!
+		// Vi kan så kjøre følgende:
 		FaceApp.layout.content.show(new FaceApp.Views.IndexView());
+		// content er en Marionette.Region som blir registrert i FaceApp.Views.Layout.
 	};
 	
 	
